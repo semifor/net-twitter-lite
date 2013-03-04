06454bfb9b3937a04e1fcc55d1183fa1efaa6ce5
Marc Mims <marc@questright.com>
2013-03-04 08:26:52 -0800

    Initial support for Twitter API v1.1 via Net::Twitter::Lite::WithAPIv1_1
    - Factored out the API method definitions into Net::Twitter::Lite::API::V1
    - Created Net::Twitter::Lite::API::V1_1 with API definitions provided by Marek Foss
      [PR# 6](https://github.com/semifor/net-twitter-lite/pull/6)
    - Created Net::Twitter::Lite::WithAPIv1_1 which uses the v1.1 API definitions
    
    V1 tests pass. Needs POD and v1.1 tests.

---
+++ lib/Net/Twitter/Lite/WithAPIv1_1.pm
@@ -0,0 +1,18 @@
+package Net::Twitter::Lite::WithAPIv1_1;
+use warnings;
+use strict;
+use parent 'Net::Twitter::Lite';
+
+sub twitter_api_def_from () { 'Net::Twitter::Lite::API::V1_1' }
+sub _default_api_url               () { 'http://api.twitter.com/1.1'  }
+sub _default_searchapiurl          () { 'http://search.twitter.com'   }
+sub _default_search_trends_api_url () { 'http://api.twitter.com/1.1'    }
+sub _default_lists_api_url         () { 'http://api.twitter.com/1.1'    }
+
+sub new {
+    my $class = shift;
+
+    return $class->SUPER::new(legacy_lists_api => 0, @_);
+}
+
+1;
