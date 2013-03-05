699d58cfb08a2e027140438c1e7860375f09b4f6
Marc Mims <marc@questright.com>
2013-03-04 18:26:30 -0800

    POD changes to accomodate Dist::Zilla builds

--- lib/Net/Twitter/Lite/WithAPIv1_1.pm
+++ lib/Net/Twitter/Lite/WithAPIv1_1.pm
@@ -3,6 +3,12 @@ use warnings;
 use strict;
 use parent 'Net::Twitter::Lite';
 
+=head1 NAME
+
+Net::Twitter::Lite::WithAPIv1_1 - A perl API library for Twitter's API v1.1
+
+=cut
+
 sub twitter_api_def_from           () { 'Net::Twitter::Lite::API::V1_1' }
 sub _default_api_url               () { 'http://api.twitter.com/1.1'    }
 sub _default_searchapiurl          () { 'http://search.twitter.com'     }
